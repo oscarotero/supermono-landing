61aeb319c6c0d45906e94540c09e98da76b85e73
Oscar Otero <oom@oscarotero.com>
2024-11-11 18:40:23 +0100

    v0

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import inline from "lume/plugins/inline.ts";
 import date from "lume/plugins/date.ts";
 import favicon from "lume/plugins/favicon.ts";
 import icons from "lume/plugins/icons.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume();
 
@@ -14,6 +15,7 @@ site.use(inline());
 site.use(date());
 site.use(favicon());
 site.use(icons());
+site.use(metas());
 site.copy("img");
 site.copy("fonts");
 
