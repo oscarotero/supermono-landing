c69ecb19ced757846d475abda17096c05927e114
Oscar Otero <oom@oscarotero.com>
2024-11-11 18:47:37 +0100

    setup url

--- _config.ts
+++ _config.ts
@@ -1,5 +1,4 @@
 import lume from "lume/mod.ts";
-import base_path from "lume/plugins/base_path.ts";
 import lightningcss from "lume/plugins/lightningcss.ts";
 import inline from "lume/plugins/inline.ts";
 import date from "lume/plugins/date.ts";
@@ -7,9 +6,10 @@ import favicon from "lume/plugins/favicon.ts";
 import icons from "lume/plugins/icons.ts";
 import metas from "lume/plugins/metas.ts";
 
-const site = lume();
+const site = lume({
+  location: new URL("https://supermo.no"),
+});
 
-site.use(base_path());
 site.use(lightningcss());
 site.use(inline());
 site.use(date());
